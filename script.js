6b7d315887b2219a5b1e1a30c49514be3bd3a0f6
Amisha2912 <amishadsce006@gmail.com>
2024-10-23 22:58:35 +0530

    created project as per instruction

---
+++ script.js
@@ -0,0 +1,21 @@
+  // Get the div element by ID
+  var div = document.getElementById("targetDiv");
+        
+  // Change background color of fetched div
+  div.style.backgroundColor = "yellow";
+  
+  // Add margin
+  div.style.margin = "20px";
+  
+  // Add padding
+  div.style.padding = "10px";
+  
+  // Change font size
+  div.style.fontSize = "18px";
+  
+  // Set font weight to bold
+  div.style.fontWeight = "bold";
+  
+  // Change height and width of the fetch div
+  div.style.height = "200px";
+  div.style.width = "300px";
